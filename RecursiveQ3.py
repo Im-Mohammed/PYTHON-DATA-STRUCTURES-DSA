e8d86012775bc4abf3c06226e298a1c14945cdd8
Im-Mohammed <mohammedkaarab.777@gmail.com>
2025-06-08 16:46:40 +0530

    New Commit

--- RecursiveQ3.py
+++ RecursiveQ3.py
@@ -3,4 +3,19 @@ Write a recursive function to print first n Even natural Numbers
 '''
 def EvenNat(n):
     if n > 0:
-        
+        EvenNat(n-1)
+        if n % 2 ==0:
+            print(n,end=" ")
+#Drive Code
+EvenNat(10)
+print('\n')
+'''
+Write a recursive function to print first n Even natural Numbers in reverse order 
+'''
+def Evenatrev(n):
+    if n > 0:
+        if n % 2 ==0:
+            print(n,end=" ")
+        Evenatrev(n-1)
+#Driver Code
+Evenatrev(10)
