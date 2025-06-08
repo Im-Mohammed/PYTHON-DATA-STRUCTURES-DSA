ae45c0c61497ea4104696dcc4dbf29c6c538bcb3
Im-Mohammed <mohammedkaarab.777@gmail.com>
2025-06-08 16:39:03 +0530

    Second Commit

--- RecursiveQ2.py
+++ RecursiveQ2.py
@@ -6,11 +6,21 @@ def Oddnat(n):
         Oddnat(n-1)
         if n % 2 !=0:
             print(n,end=" ")
+#another way 
+def Oddnat2(n):
+    if n > 0:
+        Oddnat2(n-1)
+    if n %2!=0:
+        print(n,end=" ")
+  
+print('\n')
+Oddnat2(10)
 #Driver Code 
 '''
 THe recursive case Oddnat(n-1) where as base case is n>0
 THe condition for odd is n%2!=0 
 '''
+print('\n')
 Oddnat(5)
 '''
 Write a recursive function to print first n odd natural Numbers in reverse Order
@@ -23,3 +33,14 @@ def Oddnatrev(n):
         Oddnatrev(n-1)
 #Driver Code 
 Oddnatrev(5)
+
+#another way 
+def Oddnat2rev(n):
+    if n %2!=0:
+        print(n,end=" ")
+    if n > 0:
+        Oddnat2rev(n-1)
+  
+print('\n')
+Oddnat2rev(10)
+#Driver Code 
---
+++ RecursiveQ3.py
@@ -0,0 +1,6 @@
+'''
+Write a recursive function to print first n Even natural Numbers
+'''
+def EvenNat(n):
+    if n > 0:
+        
