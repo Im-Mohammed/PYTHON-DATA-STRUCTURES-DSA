f6369301b863b673fc9ee12ddbe1e9ef4f145d16
Im-Mohammed <mohammedkaarab.777@gmail.com>
2025-06-08 22:31:44 +0530

    Updated Commit

--- Recursivesumofodd.py
+++ Recursivesumofodd.py
@@ -2,18 +2,15 @@
 Write a Recursive Function to calculate sum of First n Odd Number 
 fun(n)
 1. Task is to find sum of odd 
- if n %2 !=0:(refering to above )
 2. Recurseive case i.e n+fun(n-1)
 3. base case i.e if n > 0
 
 """
 def Oddsum(n):
-    if n >0 :
-        if n % 2 != 0:
-            s=n+Oddsum(n-1)
-        else:
-            Oddsum(n-1)
-        return s
+    if n ==1 :
+        return 1
+    s=2*n-1
+    return s + Oddsum(n-1)
 
-r=Oddsum(10)
+r=Oddsum(3)
 print("The sum of first n odd numbers is",r)
