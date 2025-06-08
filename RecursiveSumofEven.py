d2d97f83292b89587005a8f484de86a247b3b7bf
Im-Mohammed <mohammedkaarab.777@gmail.com>
2025-06-08 22:37:20 +0530

    Initial Commit

---
+++ RecursiveSumofEven.py
@@ -0,0 +1,10 @@
+'''
+Write a recursive function to calculate sum of first n even numbers 
+'''
+def Evensum(n):
+    if n == 0:
+        return 0
+    return 2*n + Evensum(n-1)
+#driver code 
+r=Evensum(3)
+print("The even sum is : ",r)
