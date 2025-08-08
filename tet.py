34765400f7a7861349fafe1d6d7233b23adea1b8
Im-Mohammed <mohammedkaarab.777@gmail.com>
2025-08-08 23:47:12 +0530

    Initial Commit

--- tet.py
+++ tet.py
@@ -174,3 +174,15 @@ def isPoweofTwo(n):
     return False
 ans=isPoweofTwo(32)
 print(ans)
+def FizzBuzz(n):
+    a=[ ]
+    for i in range(1,n+1):
+        if i % 3 == 0 and i % 5 ==0 :
+            a.append('FizzBuzz')
+        elif i % 3 ==0:
+            a.append('Fizz')
+        elif i % 5 == 0:
+            a.append('Buzz')
+        else:
+            a.append(f'i')
+    return a
